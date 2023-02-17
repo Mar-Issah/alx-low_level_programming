ae441545ef33f61a188339c683466f759dee34ed
Marsiya Issah <masy370@gmail.com>
2023-02-17 16:22:51 -0400

    Update 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints all the numbers of base 16 in lowercase
+ * You can only use the putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+for(i = 0; i < 16; i++) {
+if(i < 10) {
+putchar(i + '0');
+} 
+else
+{
+putchar(i - 10 + 'a');
+}
+}
+putchar('\n');
+return (0);
+}
