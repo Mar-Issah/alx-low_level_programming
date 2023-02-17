490bf152f597305402b0674ee6c8247c8d182299
Mar-Issah <masy370@gmail.com>
2023-02-17 10:14:08 -0800

    remove white spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - assign a random number to the variable n each time it is executed
  * if the number is greater than 0: is positive,
- * if the number is less than 0: is negative, 
+ * if the number is less than 0: is negative,
  * else the number is 0: is zero followed by a new line
  * Return: 0
  */
