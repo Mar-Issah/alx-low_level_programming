48a43dbfec5161222f84981ffc15f7acdad26211
Mar-Issah <masy370@gmail.com>
2023-02-17 15:10:17 -0800

    remove white spaces 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  * two two-digit
  * The numbers should range from 0 to 99
  * Numbers must be separated by ,followed by a space
- * All numbers should be printed with two digits. 
+ * All numbers should be printed with two digits.
  * 1 should be printed as 01
  * Return: 0
  */
