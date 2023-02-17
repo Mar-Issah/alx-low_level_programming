0cf286dc89ebc1ea2cd3062457135d815794c1d4
Marsiya Issah <masy370@gmail.com>
2023-02-17 18:54:42 -0400

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of two digits
+ * main - program that prints all possible diff combinations of two digits
  * Numbers must be separated by ,followed by a space
  * The two digits must be different
  * 01 and 10 are considered the same combination of the two digits 0 and 1
