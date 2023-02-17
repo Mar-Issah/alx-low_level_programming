4e1ea98ce308e73e3d9d858ef1fe74266b55dd56
Mar-Issah <masy370@gmail.com>
2023-02-17 11:57:37 -0800

    remove whitespace 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all single digit numbers of 
+ * main - program that prints all single digit numbers of
  * base 10 starting from
  * You are not allowed to use any variable of type char
  * You can only use the putchar function
