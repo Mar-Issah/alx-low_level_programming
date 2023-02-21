dc060c0b26cc912c939251ca371fa0655f6030d2
Marsiya Issah <masy370@gmail.com>
2023-02-21 13:11:58 -0400

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character.
+ * @c: The input character to check
+ *Returns 1 if c is a letter, lowercase or uppercase
+ *else return 0
+ * Return: int.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
