2577fb1bc4a77ac573a2335ec5eb1054bff672ef
Marsiya Issah <masy370@gmail.com>
2023-02-21 14:06:05 -0400

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,17 @@
 
 void jack_bauer(void)
 {
-	 int hours, minutes;
-    
-    for (hours = 0; hours < 24; hours++)
-    {
-        for (minutes = 0; minutes < 60; minutes++)
-        {
-            	_putchar((hours / 10) + '0');
-		_putchar((hours % 10) + '0');
-		_putchar(':');
-		_putchar((minutes / 10) + '0');
-		_putchar((minutes % 10) + '0');
-		_putchar('\n');
-        }
-    }
+int hours, minutes;
+for (hours = 0; hours < 24; hours++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+_putchar((hours / 10) + '0');
+_putchar((hours % 10) + '0');
+_putchar(':');
+_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
+_putchar('\n');
+}
+}
 }
