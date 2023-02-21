fde3a3ca6f0cebf7f4c0da9f909e5ca9bd33c647
Marsiya Issah <masy370@gmail.com>
2023-02-21 11:05:18 -0400

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints _putchar.
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	int i = 0;
+	char put_char[8] = "_putchar";
+
+	while (i < 9)
+	{
+		_putchar(put_char[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
