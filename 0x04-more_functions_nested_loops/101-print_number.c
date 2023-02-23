230e45263770e3bf460a2fba31eaf0e8c5897f10
Marsiya Issah <masy370@gmail.com>
2023-02-23 16:30:52 -0400

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer..
+ * @n: input value to check
+ * Return: nothing.
+ */
+
+void print_number(int n)
+{
+
+		_putchar(n + '0');
+}
