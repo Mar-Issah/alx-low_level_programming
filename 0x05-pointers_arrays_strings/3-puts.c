029a3f999fd1c8e08f078f2eb48564a14d685216
Marsiya Issah <masy370@gmail.com>
2023-02-27 13:31:32 -0400

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string
+ *
+ * @str: pointer to char
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	_putchar('\n');
+}
