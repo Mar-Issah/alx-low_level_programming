c2e23d99886c005a34f4ddf0b5f1eb47a03cdc4f
Marsiya Issah <masy370@gmail.com>
2023-03-01 13:16:27 -0400

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-   char *result = dest;
-    while (*dest != '\0')
-    {
-        dest++;
-    }
-    
-    while (*src != '\0')
-    {
-        *dest++ = *src++;
-    }
-    
-    *dest = '\0';
-    return (result);
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	return (dest);
 }
