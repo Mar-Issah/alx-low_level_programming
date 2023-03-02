06d60c3ff96f193a67c039bae6c8de5ae200ff58
Marsiya Issah <masy370@gmail.com>
2023-03-02 15:17:10 -0400

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,40 +8,41 @@
 
 char *cap_string(char *ch)
 {
-    int i = 0;
-    int cap_next = 1;
+int i = 0;
+int cap_next = 1;
 
-    while (*(ch + i) != '\0')
-    {
-        if (cap_next && (*(ch + i) >= 'a' && *(ch + i) <= 'z'))
-            *(ch + i) = *(ch + i) - 'a' + 'A';
+while (*(ch + i) != '\0')
+{
+if (cap_next && (*(ch + i) >= 'a' && *(ch + i) <= 'z'))
+{
+*(ch + i) = *(ch + i) - 'a' + 'A';
+}
+cap_next = 0;
 
-        cap_next = 0;
+switch (*(ch + i))
+{
+case ' ':
+case '\t':
+case '\n':
+case ',':
+case ';':
+case '.':
+case '!':
+case '?':
+case '"':
+case '(':
+case ')':
+case '{':
+case '}':
+cap_next = 1;
+break;
+default:
+break;
+}
 
-        switch (*(ch + i))
-        {
-            case ' ':
-            case '\t':
-            case '\n':
-            case ',':
-            case ';':
-            case '.':
-            case '!':
-            case '?':
-            case '"':
-            case '(':
-            case ')':
-            case '{':
-            case '}':
-                cap_next = 1;
-                break;
-            default:
-                break;
-        }
+i++;
+}
 
-        i++;
-    }
-
-    return (ch);
+return (ch);
 }
 
