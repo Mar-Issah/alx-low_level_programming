b2c8f75f2944749748a9ef67afb3313af4f4197f
Marsiya Issah <masy370@gmail.com>
2023-03-02 12:28:55 -0400

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ *
+ * @s1: pointer to char source 1
+ * @s2: pointer to char source 2
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int result;
+
+	while (*(s1 + i) != '\0')
+	{
+		if (*(s1 + i) > *(s2 + i))
+		{
+			aux = *(s1 + i) - *(s2 + i);
+			return (result);
+		}
+		else if (*(s1 + i) < *(s2 + i))
+		{
+			aux = *(s1 + i) - *(s2 + i);
+			return (result);
+		}
+		i++;
+	}
+	return (0);
+}
