27419d0f00657a64aebb4e732e14879933ae4dd7
Marsiya Issah <masy370@gmail.com>
2023-03-02 16:13:03 -0400

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,13 @@
  
  void print_number(int n)
 {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10 != 0)
-    {
-        print_number(n / 10);
-    }
-    _putchar((n % 10) + '0');
+if (n < 0) {
+_putchar('-');
+n = -n;
+}
+if (n / 10 != 0)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + '0');
 }
