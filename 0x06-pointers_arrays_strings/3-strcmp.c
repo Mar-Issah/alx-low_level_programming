85ba11e6b7d7e33b9316acf2deddd4bdea95f9ac
Marsiya Issah <masy370@gmail.com>
2023-03-02 12:30:01 -0400

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,12 +17,12 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*(s1 + i) > *(s2 + i))
 		{
-			aux = *(s1 + i) - *(s2 + i);
+			result = *(s1 + i) - *(s2 + i);
 			return (result);
 		}
 		else if (*(s1 + i) < *(s2 + i))
 		{
-			aux = *(s1 + i) - *(s2 + i);
+			result = *(s1 + i) - *(s2 + i);
 			return (result);
 		}
 		i++;
