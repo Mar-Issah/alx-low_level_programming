4181ff37c72e84f64c8afd29f92ac129e0cbf95e
Marsiya Issah <masy370@gmail.com>
2023-03-02 16:08:36 -0400

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number- function that print an interger
+ * @n: number input
+ *
+ * Return: void
+ */
+ 
+ void print_number(int n)
+{
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+    if (n / 10 != 0)
+    {
+        print_number(n / 10);
+    }
+    _putchar((n % 10) + '0');
+}
