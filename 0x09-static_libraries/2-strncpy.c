e4fc74ff7603cd4369c8521acbd9b4ee7a612eba
Marsiya Issah <masy370@gmail.com>
2023-03-10 15:13:34 -0400

    Update 2-strncpy.c

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string.
+ *
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * @n: number of bytes
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+}
