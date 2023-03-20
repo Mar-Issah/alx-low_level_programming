f0d88ad6ea2683339abfe55b034cecd56a6ebe44
Marsiya Issah <masy370@gmail.com>
2023-03-20 12:31:14 -0400

    Update dog.h to complete file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,3 +15,10 @@ typedef struct dog
 	char *owner;
 
 } dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* #ifndef DOG_H */
