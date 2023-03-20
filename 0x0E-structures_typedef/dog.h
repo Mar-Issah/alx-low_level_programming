7e179e88a950f86d7cb9af1dfbdd5d0eaa5ce102
Marsiya Issah <masy370@gmail.com>
2023-03-20 12:27:41 -0400

    Update dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struture of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+} dog_t;
