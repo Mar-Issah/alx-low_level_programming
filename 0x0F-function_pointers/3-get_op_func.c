1faf73e3e3f99454ad699dcf6fbd65a3f3ccf879
Marsiya Issah <masy370@gmail.com>
2023-03-22 17:30:28 -0400

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,17 +11,17 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {		{"+", op_add},
+	op_t ops[] = {		
+		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-
 	int i;
+	
 	i = 0;
-
 	while (i < 5)
 	{
 		if (*s == *(ops[i]).op)
