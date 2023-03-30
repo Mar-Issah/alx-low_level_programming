10dc1c515d9974156526f7e159858a428263d94e
Marsiya Issah <masy370@gmail.com>
2023-03-30 15:06:54 -0400

    Update 2-add_node.c file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,8 +24,10 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = malloc(len + 1);
 	if (new->str == NULL)
+	{
 		free(new);
 		return (NULL);
+	}
 
 	new->str = strdup(str);
 	new->len = len;
