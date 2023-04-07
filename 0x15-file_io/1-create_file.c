83a26e2a378c6edb578efc88e8a5a6a4a1f51319
Mar-Issah <masy370@gmail.com>
2023-04-07 14:53:18 -0700

    fix errors in 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * create_file - Function that creates a file
- * @filename: Name of the file to create
- * @text_content: A NULL terminated string to write to the file
- *
- * Return: 1 on success, -1 on failure
- **/
+* create_file - Function that creates a file
+* @filename: Name of the file to create
+* @text_content: A NULL terminated string to write to the file
+*
+* Return: 1 on success, -1 on failure
+**/
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    size_t len = 0;
+int fd;
+ssize_t bytes_written;
+size_t len = 0;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len] != '\0')
-            len++;
+if (text_content != NULL)
+{
+while (text_content[len] != '\0')
+len++;
 
-        bytes_written = write(fd, text_content, len);
-        if (bytes_written == -1 || (size_t)bytes_written != len)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+bytes_written = write(fd, text_content, len);
+if (bytes_written == -1 || (size_t)bytes_written != len)
+{
+close(fd);
+return (-1);
+}
+}
 
-    close(fd);
-    return (1);
+close(fd);
+return (1);
 }
