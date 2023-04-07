66a76bd8891a780be1c3ed6fdfa25294ef08b081
Mar-Issah <masy370@gmail.com>
2023-04-07 16:34:42 -0700

    fix errors in 3-set_bit.c file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * clear_bit - Function that sets the value of a bit to 0 at a given index
- * @index:  index, starting from 0 of the bit you want to set
- * Return: Return 1 if it worked, or -1 if an error occurred
- **/
+* clear_bit - Function that sets the value of a bit to 0 at a given index
+* @index:  index, starting from 0 of the bit you want to set
+* Return: Return 1 if it worked, or -1 if an error occurred
+**/
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int result;
+unsigned long int result;
 
-    if (index >= sizeof(*n) * 8)
-        return (-1);
+if (index >= sizeof(*n) * 8)
+return (-1);
 
-    result = 1UL << index;
+result = 1UL << index;
 
-    *n &= ~result;
+*n &= ~result;
 
-    return (1);
+return (1);
 }
