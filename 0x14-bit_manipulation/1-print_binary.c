23309a3010f23120585ff064ac252a817ceefd55
Mar-Issah <masy370@gmail.com>
2023-04-07 16:26:36 -0700

    fix errors in 2-get_bit.c file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
- * print_binary - Function that prints the binary representation of a number
- * @n: Number to print in binary
- **/
+* print_binary - Function that prints the binary representation of a number
+* @n: Number to print in binary
+**/
 void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
+unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
 
-    if (n == 0)
-    {
-        putchar('0');
-        return;
-    }
+if (n == 0)
+{
+putchar('0');
+return;
+}
 
-    while ((n & mask) == 0)
-        mask >>= 1;
+while ((n & mask) == 0)
+mask >>= 1;
 
-    while (mask)
-    {
-        putchar((n & mask) ? '1' : '0');
-        mask >>= 1;
-    }
+while (mask)
+{
+putchar((n & mask) ? '1' : '0');
+mask >>= 1;
+}
 }
