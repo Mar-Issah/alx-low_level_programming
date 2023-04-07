e670c762b7c817ef2af5ac47b4505f532b4af74a
Marsiya Issah <masy370@gmail.com>
2023-04-07 19:24:46 -0400

    update 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - Function that sets the value of a bit to 1 at a given index
+ * @n: Number base
+ * @index: Index to get
+ *
+ * Return: Return 1 if it worked, or -1 if an error occurred
+ **/
+int get_bit(unsigned long int n, unsigned int index)
+{
+    unsigned long int result;
+
+    if (index >= sizeof(n) * 8)
+        return (-1);
+
+    result = 1UL << index;
+
+    return ((n & result) ? 1 : 0);
+}
