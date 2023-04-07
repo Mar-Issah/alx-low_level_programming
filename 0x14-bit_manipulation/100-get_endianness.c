bc0a237ce70b55d4f3b6d7fd81e08850051fa948
Marsiya Issah <masy370@gmail.com>
2023-04-07 19:41:55 -0400

    Update 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - Function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ **/
+
+int get_endianness(void)
+{
+    unsigned int test_number = 1;
+    char *byte_pointer = (char *)&test_number;
+
+    return (*byte_pointer == 1);
+}
