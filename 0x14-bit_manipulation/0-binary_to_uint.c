9f7bee44c7f2bf264eadcffcf9cea60d554e3e59
Mar-Issah <masy370@gmail.com>
2023-04-11 14:15:11 -0700

    add text to README.md file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: Pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if
- * there is one or more chars in the string b that is not 0 or 1 b is NULL
- **/
+* binary_to_uint - Converts a binary number to an unsigned int
+* @b: Pointing to a string of 0 and 1 chars
+* Return: the converted number, or 0 if
+* there is one or more chars in the string b that is not 0 or 1 b is NULL
+**/
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
+unsigned int num = 0;
 
-    if (b == NULL)
-        return (0);
+if (b == NULL)
+return (0);
 
-    for (; *b; b++)
-    {
-        if (*b != '0' && *b != '1')
-            return (0);
+for (; *b; b++)
+{
+if (*b != '0' && *b != '1')
+return (0);
 
-        num <<= 1;
-        num |= *b - '0';
-    }
+num <<= 1;
+num |= *b - '0';
+}
 
-    return (num);
+return (num);
 }
