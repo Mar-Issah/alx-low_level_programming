83494e6407b40d0f898f972096fb616fba3a3d2b
Mar-Issah <masy370@gmail.com>
2023-05-18 10:54:29 -0400

    fix betty errors in 0-print_dlistint.c file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,14 @@
  **/
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t number = 0;
+	size_t number = 0;
 
-    while (h != NULL) {
-        printf("%d\n", h->n);
-        h = h->next;
-        number++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		number++;
+	}
 
-    return (number);
+	return (number);
 }
