8674c30b0a62adeb41b381f9931e2185b2a5a7d7
Mar-Issah <masy370@gmail.com>
2023-05-18 12:35:15 -0400

    fix betty errors in 3-add_dnodeint_end.c file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,10 +8,12 @@
  **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new_node;
+
 	if (head == NULL)
 		return NULL;
 
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return NULL;
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
-	// checks if the allocation was successful.
+
 	if (new_node == NULL)
 		return (NULL);
 
