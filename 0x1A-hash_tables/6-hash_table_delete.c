3a9efc40e93849e4a2b5afe09dedbf343aa0967a
Mar-Issah <masy370@gmail.com>
2023-06-10 09:25:28 -0400

    add new file 6-hash_table_delee.c and implement codegit

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Function that deletes a hash table
+ * @ht: Hash table
+ **/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *node = ht->array[i];
+		while (node != NULL)
+		{
+			hash_node_t *temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
