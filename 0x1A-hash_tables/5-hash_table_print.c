8333bb0b180b0aa4dca48b3fd2071a5b584db04f
Mar-Issah <masy370@gmail.com>
2023-06-10 09:14:07 -0400

    add new file 5-hash_table_print.c and implement codegit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+
+hash_table_print - Function that prints a hash table
+
+@ht: Hash table to print
+**/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int printed_entries = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (unsigned int idx = 0; idx < ht->size; idx++)
+	{
+		hash_node_t *node = ht->array[idx];
+		while (node != NULL)
+		{
+			if (printed_entries > 0)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			printed_entries++;
+			node = node->next;
+		}
+	}
+
+	printf("}\n");
+}
