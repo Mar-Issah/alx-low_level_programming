d584638e0a9289242f84be986097ef41c82bcba1
Mar-Issah <masy370@gmail.com>
2023-06-10 09:08:52 -0400

    update file 4-hash_table_set.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,19 +1,19 @@
 #include "hash_tables.h"
 /**
- * hash_table_get - Retrieves a value associated with a key
+ * hash_table_get - function that retrieves a value associated with a key
  *
  * @ht: Hash table
  * @key: Key
  *
- * Return: Value of the associated with key
+ * Return: Value of the associated with the element
  **/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	if (!ht || !key || strlen(key) == 0 || !ht->array || ht->size == 0)
-		return (NULL);
-
 	unsigned int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *current = ht->array[index];
+
+	if (!ht || !key || strlen(key) == 0 || !ht->array || ht->size == 0)
+		return (NULL);
 
 	while (current)
 	{
