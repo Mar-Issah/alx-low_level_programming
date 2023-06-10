e9595e116100047de46a4eb815c6fc1784441c34
Mar-Issah <masy370@gmail.com>
2023-06-10 09:27:23 -0400

    update file 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,15 +7,18 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned int i;
+
 	if (ht == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		hash_node_t *node = ht->array[i];
+
 		while (node != NULL)
 		{
 			hash_node_t *temp = node;
+
 			node = node->next;
 			free(temp->key);
 			free(temp->value);
